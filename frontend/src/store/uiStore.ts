6ee1bf02e6e50027f8583880143087becfbec934
robel <robelyinager12@gmail.com>
2026-08-31 20:33:53 +0300

    add openSidebar action for completeness

--- frontend/src/store/uiStore.ts
+++ frontend/src/store/uiStore.ts
@@ -4,10 +4,12 @@ interface UiState {
   isSidebarOpen: boolean;
   toggleSidebar: () => void;
   closeSidebar: () => void;
+  openSidebar: () => void;
 }
 
 export const useUiStore = create<UiState>((set) => ({
   isSidebarOpen: false,
   toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
   closeSidebar: () => set({ isSidebarOpen: false }),
+  openSidebar: () => set({ isSidebarOpen: true }),
 }));
